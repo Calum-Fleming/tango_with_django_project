108d485a7b943a897553acdb295945cc125ce41c
Calum Fleming <2594251f@student.gla.ac.uk>
2021-07-26 17:56:19 +0100

    template dirrectory added

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tango_with_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['<workspace/tango_with_django'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
