1b01b9c87d6ed6e07ed1102f1f13e0175e6611f9
Calum Fleming <2594251f@student.gla.ac.uk>
2021-07-29 12:17:28 +0100

    add category form added

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -29,4 +31,19 @@ def index(request):
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Calum Fleming, 2594251f'}
-    return render(request, 'rango/about.html', context=context_dict)  
+    return render(request, 'rango/about.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+    if form.is_valid():
+        form.save(commit=True)
+        return redirect('/rango/')
+    else:
+        print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
